0cc4f660b2987a9c147976f63c66e25cadd54191
Andreas Umbach <umbach@gmail.com>
2016-05-12 11:22:58 +0200

    rollback zu 2d version

--- Point.java
+++ Point.java
@@ -1,7 +1,7 @@
 
 class Point {
-  float x, y, w;
-  Point(float x, float y) { this.x = x; this.y = y; this.w = 1; }
+  float x, y;
+  Point(float x, float y) { this.x = x; this.y = y; }
   
   public void transform(Matrix2x2 m)
   {
@@ -9,15 +9,5 @@ class Point {
     float newy = m.m[2] * x + m.m[3] * y;
     x = newx;
     y = newy;
-  }
-  
-  public void transform(Matrix3x3 m)
-  {
-    float newx = m.m[0] * x + m.m[1] * y + m.m[2] * w;
-    float newy = m.m[3] * x + m.m[4] * y + m.m[5] * w;
-    float neww = m.m[6] * x + m.m[7] * y + m.m[8] * w;
-    x = newx;
-    y = newy;
-    w = neww;
   }
 }
